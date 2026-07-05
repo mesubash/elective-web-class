574e2c32a39914468cbfb0e4823ee404b1aefe06
Subash Singh Dhami <subashdhamee@gmail.com>
2026-07-05 08:24:02 +0545

    initial setup

---
+++ src/main/java/io/github/subash/electiveclass/ElectiveClassApplication.java
@@ -0,0 +1,13 @@
+package io.github.subash.electiveclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectiveClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectiveClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/subash/electiveclass/ElectiveClassApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.subash.electiveclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectiveClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
